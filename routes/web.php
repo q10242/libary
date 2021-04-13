68a87b10020b66f8a14e4d47b2e312c33f99d586
q10242 <q10242@gmail.com>
2021-04-13 09:08:12 +0800

    adds the book resource and testing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BooksController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/books', [BooksController::class,'store']);
+Route::patch('/books/{book}', [BooksController::class,'update']);
